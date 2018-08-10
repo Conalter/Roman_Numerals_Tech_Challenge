0e5d6eca149df389ac550d78694abfdc0dd45284
Lyle Mitchell <ljwmitchell@gmail.com>
2018-08-10 22:06:54 +0100

    adds basic functionality

---
+++ Roman_Numerals_Tech_Challenge.js
@@ -0,0 +1,22 @@
+var numeral = 9;
+
+if(numeral === 1) {
+  console.log("I");
+} else if (numeral === 2){
+  console.log("II");
+} else if (numeral === 3){
+  console.log("III");
+} else if (numeral === 4){
+  console.log("IV");
+} else if (numeral === 5){
+  console.log("V");
+} else if (numeral === 6){
+  console.log("VI");
+} else if (numeral === 7){
+  console.log("VII");
+} else if (numeral === 8){
+  console.log("VIII");
+} else if (numeral === 9){
+  console.log("IX");
+} else if (numeral === 10)
+console.log("X");
