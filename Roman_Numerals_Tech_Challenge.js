f2a4aa5792f9ea99d8be50917c048232a6128c5a
Lyle Mitchell <ljwmitchell@gmail.com>
2018-08-11 14:07:02 +0100

    adds functionality

--- Roman_Numerals_Tech_Challenge.js
+++ Roman_Numerals_Tech_Challenge.js
@@ -1,22 +1,29 @@
-var numeral = 9;
+// // basic if statement for converting int into Roman numeral string:
+//
+var NumGenerator = function(numeral){
+      if (numeral === 1){
+      console.log("I");
+    } else if (numeral === 2){
+      console.log("II");
+    } else if (numeral === 3){
+      console.log("III");
+    } else if (numeral === 4){
+      console.log("IV");
+    } else if (numeral === 5){
+      console.log("V");
+    } else if (numeral === 6){
+      console.log("VI");
+    } else if (numeral === 7){
+      console.log("VII");
+    } else if (numeral === 8){
+      console.log("VIII");
+    } else if (numeral === 9){
+      console.log("IX");
+    } else if (numeral === 10){
+      console.log("X");
+    } else {
+      console.log("Please try again.");
+    }
+  }
 
-if(numeral === 1) {
-  console.log("I");
-} else if (numeral === 2){
-  console.log("II");
-} else if (numeral === 3){
-  console.log("III");
-} else if (numeral === 4){
-  console.log("IV");
-} else if (numeral === 5){
-  console.log("V");
-} else if (numeral === 6){
-  console.log("VI");
-} else if (numeral === 7){
-  console.log("VII");
-} else if (numeral === 8){
-  console.log("VIII");
-} else if (numeral === 9){
-  console.log("IX");
-} else if (numeral === 10)
-console.log("X");
+  NumGenerator(0);
