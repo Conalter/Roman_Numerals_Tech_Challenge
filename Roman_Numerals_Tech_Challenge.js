f989635e8cedb428ed80d2e681660d18f9c8fdb0
Lyle Mitchell <ljwmitchell@gmail.com>
2018-08-11 20:27:09 +0100

    adds different version using for loop

--- Roman_Numerals_Tech_Challenge.js
+++ Roman_Numerals_Tech_Challenge.js
@@ -1,29 +1,51 @@
-// // basic if statement for converting int into Roman numeral string:
-//
-var NumGenerator = function(numeral){
-      if (numeral === 1){
-      console.log("I");
-    } else if (numeral === 2){
-      console.log("II");
-    } else if (numeral === 3){
-      console.log("III");
-    } else if (numeral === 4){
-      console.log("IV");
-    } else if (numeral === 5){
-      console.log("V");
-    } else if (numeral === 6){
-      console.log("VI");
-    } else if (numeral === 7){
-      console.log("VII");
-    } else if (numeral === 8){
-      console.log("VIII");
-    } else if (numeral === 9){
-      console.log("IX");
-    } else if (numeral === 10){
-      console.log("X");
-    } else {
-      console.log("Please try again.");
+// Basic function with if statement for converting int into Roman numeral string:
+
+// var NumGenerator = function(numeral){
+//   if (numeral === 1){
+//     console.log("I");
+//   } else if (numeral === 2){
+//     console.log("II");
+//   } else if (numeral === 3){
+//     console.log("III");
+//   } else if (numeral === 4){
+//     console.log("IV");
+//   } else if (numeral === 5){
+//     console.log("V");
+//   } else if (numeral === 6){
+//     console.log("VI");
+//   } else if (numeral === 7){
+//     console.log("VII");
+//   } else if (numeral === 8){
+//     console.log("VIII");
+//   } else if (numeral === 9){
+//     console.log("IX");
+//   } else if (numeral === 10){
+//     console.log("X");
+//   } else {
+//     console.log("Please try again");
+//   }
+// }
+
+// NumGenerator(9);
+
+// More complicated version using a for loop to convert decimal into roman numeral:
+
+var NumGenerator = function(num){
+  var result = '';
+  // Two arrays, one with decimal number & the other with of the Roman equivalent.
+  var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"]
+  // Loops over the arrays
+  for (var i = 0;i<=decimal.length;i++) {
+    // Maintain loop until decimal is less than the Roman numeral
+    while (num%decimal[i] < num) {
+      // Adds Roman numeral to result string
+      result += roman[i];
+      // Subtracts decimal value of Roman Numeral from decimal number.
+      num -= decimal[i];
     }
   }
-
-  NumGenerator(0);
+  console.log(result);
+}
+// Number to be converted
+NumGenerator(1987);
