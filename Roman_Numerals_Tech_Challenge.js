a3c368bdc0c45c5e83d69caff025903e2165e40a
Lyle Mitchell <ljwmitchell@gmail.com>
2018-08-12 10:23:28 +0100

    adds if statement to check wether numbers are within parameters

--- Roman_Numerals_Tech_Challenge.js
+++ Roman_Numerals_Tech_Challenge.js
@@ -21,31 +21,36 @@
 //     console.log("IX");
 //   } else if (numeral === 10){
 //     console.log("X");
-//   } else {
+//   } else if ((numeral < 1) || (numeral > 3999)) {
 //     console.log("Please try again");
 //   }
 // }
-
+//
 // NumGenerator(9);
 
 // More complicated version using a for loop to convert decimal into roman numeral:
 
 var NumGenerator = function(num){
   var result = '';
-  // Two arrays, one with decimal number & the other with of the Roman equivalent.
-  var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  var roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"]
-  // Loops over the arrays
-  for (var i = 0;i<=decimal.length;i++) {
-    // Maintain loop until decimal is less than the Roman numeral
-    while (num%decimal[i] < num) {
-      // Adds Roman numeral to result string
-      result += roman[i];
-      // Subtracts decimal value of Roman Numeral from decimal number.
-      num -= decimal[i];
+  // Checks is number is within paramaters
+  if ((num < 1) || (num > 3999)) {
+    console.log("Please try again");
+  } else {
+    // Two arrays, one with decimal number & the other with of the Roman equivalent.
+    var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"]
+    // Loops over the arrays
+    for (var i = 0;i<=decimal.length;i++) {
+      // Maintain loop until decimal is less than the Roman numeral
+      while (num%decimal[i] < num) {
+        // Adds Roman numeral to result string
+        result += roman[i];
+        // Subtracts decimal value of Roman Numeral from decimal number.
+        num -= decimal[i];
+      }
     }
   }
   console.log(result);
 }
 // Number to be converted
-NumGenerator(1987);
+NumGenerator(899090);
